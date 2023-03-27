1945749e4c1bfae6e1162424138494dd4b1a4015
oyunmintrio95 <oyunmintrio95@gmail.com>
2023-03-27 20:15:25 +0900

    subsequence completed

---
+++ src/day2/day2_subsequence.java
@@ -0,0 +1,40 @@
+package day2;
+
+public class day2_subsequence {
+    public static boolean isSubsequence(String s, String t) {
+
+        int s_length = s.length();
+        int t_length = t.length();
+
+        char[] array_s = s.toCharArray();
+        char[] array_t = t.toCharArray();
+
+        int i = 0;
+        int j = 0;
+
+        if (s_length < 1){
+            return true;
+        }
+
+
+        while(i<t_length){
+            if(array_t[i]==array_s[j]){
+                j++;
+            }
+            i++;
+
+            if(j == s_length){
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    public static void main(String[] args) {
+        String s = "abc";
+        String t = "ahbgdc";
+
+        Boolean isSubsequence = isSubsequence(s, t);
+    }
+}
