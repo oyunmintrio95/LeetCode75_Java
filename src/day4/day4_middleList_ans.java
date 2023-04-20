7aa73c753efb12cd453a55c3b1f149f797d17d8b
oyunmintrio95 <oyunmintrio95@gmail.com>
2023-04-20 16:13:20 -0700

    day4 a better answer

---
+++ src/day4/day4_middleList_ans.java
@@ -0,0 +1,15 @@
+package day4;
+
+import day3.ListNode;
+
+public class day4_middleList_ans {
+    public ListNode middleNode(ListNode head) {
+        ListNode t1 = head;
+        ListNode t2 = head;
+        while(t2!=null && t2.next!=null){
+            t1 = t1.next;
+            t2 = t2.next.next;
+        }
+        return t1;
+    }
+}
