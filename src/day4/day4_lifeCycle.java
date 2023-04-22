c5a8b3a85eaa2a2c5650cec196cec216a4dcc656
oyunmintrio95 <oyunmintrio95@gmail.com>
2023-04-21 22:57:02 -0700

    day4

---
+++ src/day4/day4_lifeCycle.java
@@ -0,0 +1,28 @@
+package day4;
+
+import day3.ListNode;
+
+public class day4_lifeCycle {
+
+    public ListNode detectCycle(ListNode head) {
+        // Initialize two pointers, slow and fast, to the head of the linked list.
+        ListNode slow = head;
+        ListNode fast = head;
+
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+            if (slow == fast) {
+
+                slow = head;
+                while (slow != fast) {
+                    slow = slow.next;
+                    fast = fast.next;
+                }
+                return slow;
+            }
+        }
+
+        return null;
+    }
+}
