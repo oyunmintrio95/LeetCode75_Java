e042966c505a73caf6fbf253bba320fdbeaf9fa6
oyunmintrio95 <oyunmintrio95@gmail.com>
2023-04-26 14:48:37 -0700

    day6_1 04/26/23

---
+++ src/day6/day6_nthChild.java
@@ -0,0 +1,41 @@
+package day6;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Stack;
+
+class Node {
+    public int val;
+    public List<Node> children;
+
+    public Node() {}
+
+    public Node(int _val) {
+        val = _val;
+    }
+
+    public Node(int _val, List<Node> _children) {
+        val = _val;
+        children = _children;
+    }
+};
+public class day6_nthChild {
+    //Preorder traversal is a type of depth-first search(DFS)
+    //Children are processed from left to right.
+
+    //A new default argument for the function to hold our answer array
+    //create a list for output
+    List<Integer> output = new ArrayList<>();
+    public List<Integer> preorder(Node root) {
+        // If the given root is null, then the output would be null
+        if(root == null) return output;
+        output.add(root.val);
+
+        for(Node child: root.children){
+            preorder(child);
+        }
+
+        return output;
+
+    }
+}
