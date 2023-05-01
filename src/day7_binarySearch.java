9c44d69f74ba2293fdc90de7b6318befa90d20ea
oyunmintrio95 <oyunmintrio95@gmail.com>
2023-05-01 12:59:01 -0700

    day7 binary search 05/01

---
+++ src/day7_binarySearch.java
@@ -0,0 +1,22 @@
+public class day7_binarySearch {
+    public int search(int[] nums, int target) {
+        int start = 0;
+        int end = nums.length - 1;
+
+        while ( start <= end){
+            int mid = start + (end-start)/2;
+
+            if(nums[mid] == target) return mid;
+            else if (target < nums[mid]){
+                //trget exist left side of the middle index, redeclare the end.
+                end = mid - 1;
+            }else{
+                //trget exist right side of the middle index, redeclare the start.
+                start = mid+1;
+            }
+        }
+
+        return -1;
+
+    }
+}
