25bea071b18a7c63e26a5518eb1b9697922acffd
oyunmintrio95 <oyunmintrio95@gmail.com>
2023-06-07 20:22:18 -0700

    0607 2.AddTwo Number

---
+++ src/from1_to_10/AddTowNumber2.java
@@ -0,0 +1,31 @@
+package from1_to_10;
+
+import day3.ListNode;
+
+public class AddTowNumber2 {
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        ListNode dummy = new ListNode(0);
+        ListNode curr = dummy;
+        int carry = 0;
+
+        while(l1!=null || l2 != null || carry ==1){
+            int sum = 0;
+            if(l1 != null){
+                sum += l1.val;
+                l1 = l1.next;
+            }
+            if(l2 != null){
+                sum += l2.val;
+                l2 = l2.next;
+            }
+            sum += carry;
+            carry = sum / 10;
+            ListNode node = new ListNode(sum %10);
+            curr.next = node;// curr will point to the new node if we get
+            curr = curr.next; // update current every time
+
+        }
+        return dummy.next;
+
+    }
+}
