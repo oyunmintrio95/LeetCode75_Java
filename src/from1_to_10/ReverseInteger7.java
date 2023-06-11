4c01ee3b10bc69d48ecba2c2fd8b11dc738d73bb
oyunmintrio95 <oyunmintrio95@gmail.com>
2023-06-10 19:20:40 -0700

    interview question 7

---
+++ src/from1_to_10/ReverseInteger7.java
@@ -0,0 +1,26 @@
+package from1_to_10;
+
+public class ReverseInteger7 {
+    public int reverse(int x) {
+        long finalNum =0;
+
+        while(x!=0){
+
+            int lastDigit = x % 10;
+            finalNum += lastDigit;
+            finalNum = finalNum*10;
+            x = x/10;
+        }
+
+        finalNum = finalNum/10;
+        if(finalNum > Integer.MAX_VALUE || finalNum < Integer.MIN_VALUE){
+            return 0;
+        }
+        if (x<0){
+            return (int)(-1*finalNum);
+        }
+
+        return (int)finalNum;
+
+    }
+}
